d93d9afbf334386152068598770ad45c26fe56df
shuij <864805002@qq.com>
2023-12-06 19:02:31 +0800

    init commint

---
+++ entry/src/main/ets/common/constants/CommonConstants.js
@@ -0,0 +1,107 @@
+export default class CommonConstants {
+}
+/**
+ * The index page message
+ */
+CommonConstants.INDEX_MESSAGE = 'Index Page';
+/**
+ * The src msg.
+ */
+CommonConstants.SECOND_SRC_MSG = 'Index页面传来的数据';
+/**
+ * The second page url.
+ */
+CommonConstants.SECOND_URL = 'pages/SecondPage';
+/**
+ * The second page message.
+ */
+CommonConstants.SECOND_MESSAGE = 'Second Page';
+/**
+ * The src param.
+ */
+CommonConstants.SECOND_SRC_PARAM = 'src';
+/**
+ * Full the width.
+ */
+CommonConstants.FULL_WIDTH = '100%';
+/**
+ * Full the height.
+ */
+CommonConstants.FULL_HEIGHT = '100%';
+/**
+ * The hello world font size.
+ */
+CommonConstants.FONT_SIZE = '38fp';
+/**
+ * The param font size.
+ */
+CommonConstants.PARAMS_FONT_SIZE = '20fp';
+/**
+ * The button font size.
+ */
+CommonConstants.BUTTON_FONT_SIZE = '16fp';
+/**
+ * The button width.
+ */
+CommonConstants.BUTTON_WIDTH = '296vp';
+/**
+ * The button height.
+ */
+CommonConstants.BUTTON_HEIGHT = '40vp';
+/**
+ * The layout height.
+ */
+CommonConstants.LAYOUT_HEIGHT = '140vp';
+/**
+ * The param opacity.
+ */
+CommonConstants.PARAMS_OPACITY = 0.6;
+/**
+ * Input length of the account.
+ */
+CommonConstants.INPUT_ACCOUNT_LENGTH = 11;
+/**
+ *  Input length of the password.
+ */
+CommonConstants.INPUT_PASSWORD_LENGTH = 8;
+/**
+ *  Left padding of the input box
+ */
+CommonConstants.INPUT_PADDING_LEFT = 0;
+/**
+ * Delay time of simulated login
+ */
+CommonConstants.LOGIN_DELAY_TIME = 2000;
+/**
+ * Common Spacing of Components
+ */
+CommonConstants.COMMON_SPACE = 12;
+/**
+ * Title text of the home page
+ */
+CommonConstants.HOME_TITLE = '首页';
+/**
+ * Title text of the setting page
+ */
+CommonConstants.MINE_TITLE = '我的';
+/**
+ * Spacing of other login methods
+ */
+CommonConstants.LOGIN_METHODS_SPACE = 44;
+/**
+ * The width or height of the component is spread across the parent component.
+ */
+CommonConstants.FULL_PARENT = '100%';
+/**
+ * The width of setting list
+ */
+CommonConstants.SET_LIST_WIDTH = '42%';
+/**
+ * Home tab index
+ */
+CommonConstants.HOME_TAB_INDEX = 0;
+/**
+ * Mine tab index
+ */
+CommonConstants.MINE_TAB_INDEX = 1;
+//# sourceMappingURL=CommonConstants.js.map
